7cfdf2ef9d17289eda0f2616b3437c742c20ea98
yj <yjchoi9212@gmail.com>
2026-03-24 00:49:42 +0900

    add data loading in train.py

--- train.py
+++ train.py
@@ -1,4 +1,12 @@
+from data_loader import get_dataloaders
 import torch
 
 def train(config):
+    train_loader, val_loader = get_dataloaders(
+        config["csv_path"],
+        config["batch_size"],
+        config["train_fraction"],
+        config["shuffle_train"],
+        config["num_workers"],
+    )
     pass
